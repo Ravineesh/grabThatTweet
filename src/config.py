4acfe2d5b4922a89ea83378c6afc6c2e376e0b99
Ravineesh <ravineesh04@gmail.com>
2021-08-26 20:51:51 +0200

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,18 @@
+FILE_NAME = 'tweet_data.csv'
+
+user_name = []
+user_id = []
+user_screen_name = []
+source = []
+language = []
+tweet_text = []
+tweet_creation_date = []
+retweets_count = []
+like_count = []
+hashtag = []
+user_mention = []
+
+# Twitter Columns
+tweet_columns = ['user_name', 'user_id', 'user_screen_name', 'source',
+                 'language', 'tweet_text', 'tweet_creation_date', 'retweets_count',
+                 'likes_count', 'hashtag', 'user_mention']
