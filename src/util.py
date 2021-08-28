af17a5e2d2fc4188b64a50a99c571ade8ba9d3ef
Ravineesh <ravineesh04@gmail.com>
2021-08-28 17:37:16 +0200

    Creating util.py
    Adding functions for extracting hashtags and user_mentions and to write output.

---
+++ src/util.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import os
+import config
+
+
+def extract_hash_tags(hash_tag_entries):
+    hash_tag_list = []
+    hashtag = ""
+    if len(hash_tag_entries) > 0:
+        for entry in range(0, len(hash_tag_entries)):
+            hashtag = hashtag + hash_tag_entries[entry]['text'] + ','
+        hash_tag_list.append(hashtag)
+    else:
+        hash_tag_list.append("#none")
+
+    return hash_tag_list
+
+
+def extract_user_mention(user_mention_entries):
+    user_mention_list = []
+    user_mention = ""
+    if len(user_mention_entries) > 0:
+        for entry in range(0, len(user_mention_entries)):
+            user_mention = user_mention + user_mention_entries[entry]['screen_name'] + ','
+        user_mention_list.append(user_mention)
+    else:
+        user_mention_list.append("#none")
+
+    return user_mention_list
+
+
+def write_output(tweet_data, output_dir):
+    df = pd.DataFrame(tweet_data, columns=config.tweet_columns)
+    print('Total number of tweet fetched are', len(df))
+    df.to_csv(os.path.join(output_dir, config.FILE_NAME), index=False, header=True)
